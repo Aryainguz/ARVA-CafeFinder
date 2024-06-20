e5341051cf8f06f158f98f853c1829bb9b19923c
Aryainguz <aryaninguz369@gmail.com>
2024-06-20 11:46:48 +0530

    chore: Update CafeItemCard component with google places api

--- client/components/ui/CafeItemCard.tsx
+++ client/components/ui/CafeItemCard.tsx
@@ -1,4 +1,4 @@
-import { MapPin, Star } from "lucide-react";
+import { MapPin, Star, Users } from "lucide-react";
 import Image from "next/image";
 
 const CafeItemCard = ({ cafe,showDetails }: any) => {
@@ -20,11 +20,9 @@ onClick={()=>showDetails(cafe)}
             height={100}
           />
         <div className="p-5">
-          <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               {cafe.name}
             </h5>
-          </a>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             <MapPin size={20} className="inline mr-2" />
             {cafe.formatted_address.split(",")[0] +
@@ -39,29 +37,9 @@ onClick={()=>showDetails(cafe)}
           </p>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             <Star size={20} className="inline mr-2" />
-            {cafe.rating}
+            {cafe.rating} <Users size={20} className="inline ml-4" /> {cafe?.user_ratings_total}
           </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-amber-700 rounded-lg hover:bg-amber-800 focus:ring-4 focus:outline-none focus:ring-amber-300 dark:bg-amber-600 dark:hover:bg-amber-700 dark:focus:ring-amber-800"
-          >
-            View Cafe
-            <svg
-              className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 14 10"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M1 5h12m0 0L9 1m4 4L9 9"
-              />
-            </svg>
-          </a>
+
         </div>
       </div>
     </>
