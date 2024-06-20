c6d1e7194b0af4806fce0c2fd28683c9f3c8e38b
Aryainguz <aryaninguz369@gmail.com>
2024-06-20 19:30:19 +0530

    chore: Add lazy loading for images and update layout

--- client/app/find/page.tsx
+++ client/app/find/page.tsx
@@ -1,9 +1,24 @@
+"use client"
+import Cafes from '@/components/Cafes'
 import FindHero from '@/components/FindHero'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+  const [cafesList,setCafesList] = useState([])
+
+  useEffect(() => {
+    getCafeLists()
+  }, [])
+
+  const getCafeLists = async () => {
+    const res = await fetch('/api/google-place-api?q=cafes in bangalore')
+    const data = await res.json()
+    setCafesList(data.data.results)
+  } 
   return (
-    <div>
+    <>
       <FindHero/>
-    </div>
+      <Cafes cafesList={cafesList}/>
+      </>
   )
 }
